68192a44296a63ac001191c3d7b65eedd896a57a
vSolomony <mohamed.a.kassem95@gmail.com>
2023-10-02 19:11:17 -0700

    test

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+/**
+ * check_cycle - to check for cycle
+ * @list: structure singly linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *rabbit;
+
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+
+	turtle = list;
+	rabbit = list;
+
+	while (rabbit != NULL && rabbit->next != NULL)
+	{
+		turtle = turtle->next;
+		rabbit = rabbit->next->next;
+
+		if (turtle == rabbit)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
