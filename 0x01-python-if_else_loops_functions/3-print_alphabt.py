6adce819550b7e46e5e92ba28dcc788d8406bc56
vSolomony <mohamed.a.kassem95@gmail.com>
2023-10-03 18:02:47 -0700

    123

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(letter) == "q" or chr(letter) == "e":
+        continue
+    else:
         print("{}".format(chr(letter)), end="")
