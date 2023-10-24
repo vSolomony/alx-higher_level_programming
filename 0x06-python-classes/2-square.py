9cf29a02fe15658ed10dd94ddff13cb249aa061d
vSolomony <mohamed.a.kassem95@gmail.com>
2023-10-24 16:10:42 -0700

    123

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square class defination."""
+
+
+class Square:
+    """Square class body"""
+
+    def __init__(self, size=0):
+        """Square class contructor
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
