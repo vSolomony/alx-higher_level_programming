fcf2b0ac0a9d1291084bc3389806229030c35aff
vSolomony <mohamed.a.kassem95@gmail.com>
2023-11-07 02:17:13 -0800

    123

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-""" My class module
-"""
+"""Defines a Python class-to-JSON function."""
 
-class MyClass:
-    """ My class
-    """
 
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
-
-    def __str__(self):
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
